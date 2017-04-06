f6c2092c86224a5d00cbd062e34901251b73ccc3
cqa262416 <cqa262416@gmail.com>
2017-04-06 14:16:20 +0800

    计算时间调整

--- 2017-04-05_计算日期的差值/xiaoyao/solution.py
+++ 2017-04-05_计算日期的差值/xiaoyao/solution.py
@@ -4,10 +4,10 @@ from datetime import datetime, timedelta, tzinfo, timezone
 
 def date_distance(date_str):
     tz = timezone(timedelta(hours=8))
-    now_date = datetime.now(tz)
+    now_date = datetime.now(tz).date()
     fmt = '%Y-%m-%d'
     post_date = datetime.strptime(date_str, fmt)
-    post_date = post_date.replace(tzinfo=tz)
+    post_date = post_date.replace(tzinfo=tz).date()
     return (post_date - now_date).days
 
 if __name__ == '__main__':

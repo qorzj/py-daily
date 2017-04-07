084e917334ce206562792554876e2e1b562453a6
kevin12370 <2104605213@qq.com>
2017-04-07 12:51:36 +0800

    zhangxiaobo

--- 2017-04-07_电话号码解析/zhangxiaobo/solution.py
+++ 2017-04-07_电话号码解析/zhangxiaobo/solution.py
@@ -2,24 +2,20 @@ import re
 
 
 def parse_to_tuple(s):
-    partten = re.compile(r'\(([\d]+)\)(\d+)@(.+)')
-    if len(partten.findall(s)) == 0:
+    partten = re.compile(r'\((\d+)\)(\d+)+@(.+)')
+    if partten.search(s) is None:
         return '正确格式为 (023)68001111@office '
     else:
-        return partten.findall(s)[0]
+        return partten.search(s).groups()
 
 
 def parse_to_dict(s):
     partten = re.compile(r'\(([\d]+)\)(\d+)@(.+)')
-    info = {}
-    result = partten.findall(s)
-    if len(result) == 0:
+    result = partten.search(s)
+    if result is None:
         return '正确格式为 (023)68001111@office '
     else:
-        info['area'] = result[0][0]
-        info['tel'] = result[0][1]
-        info['addr'] = result[0][2]
-        return info
+        return dict(zip(('area', 'tel', 'addr'), result.groups()))
 
 print(parse_to_tuple('(023)68001111@office'))
 print(parse_to_dict('(023)68001111@office'))

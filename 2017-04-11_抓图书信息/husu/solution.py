d7ec729838a58533b3ab0bf275dada307444efe0
husu <azhong_husu@hotmail.com>
2017-04-14 00:01:28 +0800

    完成抓取的作业

--- 2017-04-11_抓图书信息/husu/solution.py
+++ 2017-04-11_抓图书信息/husu/solution.py
@@ -1,2 +1,14 @@
+# coding=utf-8
+import requests
+
+
 def book_info(url):
-    return ""
+    txt = requests.get(url).text
+    key1 = '<meta name="keywords" content="'
+    idx = txt.find(key1) + len(key1)
+    tmp = txt[idx::]
+    bookInf = tmp[0:tmp.find('">')].split(',')
+    return {'name': bookInf[0], 'author': bookInf[1], 'press': bookInf[2]}
+
+
+print(book_info("https://book.douban.com/subject/3227098/"))

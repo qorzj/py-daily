32e898f6577ca9ab597f5783f47b891d3b69aa9e
cqa262416 <cqa262416@gmail.com>
2017-04-13 10:29:49 +0800

    逍遥＿抓图书信息2

--- 2017-04-11_抓图书信息/xiaoyao/solution.py
+++ 2017-04-11_抓图书信息/xiaoyao/solution.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3.5
 #-*- coding:utf-8 -*-
-import requests
+import requests,re
 from lxml import etree
 def book_info(url):
-    response = requests.get(url)
-    html = etree.HTML(response.text)
-    segs = html.xpath('//span')
-    return segs
+    text = requests.get(url).text
+    div = re.search('[\s\S]+(<div[\s\S]*?作者[\s\S]*?出版社[\s\S]*?副标题[\s\S]*?</div>)', text).group(1)
+    div = re.sub('\n|,|&nbsp;', '', div)
+    div = re.findall('>([\s\S]+?)<', div)
+    div = list(filter(lambda x: x.strip(), div))
+    div = [re.sub('\s', '', i) for i in div]
+    return div
 
 if __name__ == '__main__':
     url = 'https://book.douban.com/subject/3227098/'

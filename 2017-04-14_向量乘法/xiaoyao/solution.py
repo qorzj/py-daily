1cbf00d4ed1e1b871b363bc81b32bd48abd0894b
cqa262416 <cqa262416@gmail.com>
2017-04-14 14:13:24 +0800

    逍遥＿向量乘法

---
+++ 2017-04-14_向量乘法/xiaoyao/solution.py
@@ -0,0 +1,44 @@
+class Vector(list):
+    """
+        >>> Vector(1, 2, 3) * Vector(2, 3, 4)
+        Vector(2, 6, 12)
+        >>> v = Vector(3,4,5,6)
+        >>> v * 2
+        Vector(6, 8, 10, 12)
+        >>> v[0]
+        3
+        >>> v[-1]
+        6
+        >>> len(v)
+        4
+        >>> list(v)
+        [3, 4, 5, 6]
+        >>> str(v)
+        'Vector(3, 4, 5, 6)'
+        >>> sum(v)
+        18
+    """
+    def __init__(self, *args):  # known special case of list.__init_
+        if isinstance(args[0], list) or isinstance(args[0], tuple):
+            list.__init__(self, args[0])
+        else:
+            list.__init__(self, args)
+
+    def __mul__(self, n):
+        if isinstance(n, int):
+            return Vector([i * n for i in self])
+        elif isinstance(n, type(self)):
+            return Vector([self[i] * n[i] for i in range(len(self))])
+        else:
+            raise TypeError('invalid args')
+
+    def __repr__(self, *args, **kwargs):
+        return self.__str__()
+
+    def __str__(self):
+        return "Vector(%s)" % ', '.join(map(str, self))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()

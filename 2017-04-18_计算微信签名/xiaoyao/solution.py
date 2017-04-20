76aecb9e5c0cf203212a3439109eb6c7d62d34cd
shawn <837364078@qq.com>
2017-04-20 12:46:54 +0800

    逍遥_webchat签名

--- 2017-04-18_计算微信签名/xiaoyao/solution.py
+++ 2017-04-18_计算微信签名/xiaoyao/solution.py
@@ -7,33 +7,18 @@ import time
 chars = string.digits + string.ascii_letters
 
 
-def get_signature(ticket, url):
-    user_params = dict()
-    user_params["noncestr"] = get_noncestr()
-    user_params["jsapi_ticket"] = ticket
-    user_params["timestamp"] = get_timestamp()
-    user_params["url"] = url
-    tmp = ''
-    for key in sorted(user_params.keys()):
-        tmp += '&' + key + '=' + user_params[key]
-    return hashlib.sha1(tmp[1:].encode()).hexdigest()
-
-
-def get_signature_test():
+def get_signature(url, ticket, timestamp=None, noncestr=None, ):
     """
-    >>> get_signature_test()
+    >>> get_signature('http://mp.weixin.qq.com?params=value',\
+    'sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg','1414587457','Wm3WZYTPz0wzccnW')
     '0f9de62fce790f9a083d5c99e95740ceb90c27ed'
     """
     user_params = dict()
-    user_params["noncestr"] = 'Wm3WZYTPz0wzccnW'
-    user_params[
-        "jsapi_ticket"] = 'sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg'
-    user_params["timestamp"] = '1414587457'
-    user_params["url"] = 'http://mp.weixin.qq.com?params=value'
-    tmp = ''
-    for key in sorted(user_params.keys()):
-        tmp += '&' + key + '=' + user_params[key]
-    return hashlib.sha1(tmp[1:].encode()).hexdigest()
+    user_params["url"] = url
+    user_params["jsapi_ticket"] = ticket
+    user_params["timestamp"] = get_timestamp() if not timestamp else timestamp
+    user_params["noncestr"] = get_noncestr() if not noncestr else noncestr
+    return hashlib.sha1('&'.join(k+'='+v for k, v in sorted(user_params.items())).encode()).hexdigest()
 
 
 def get_noncestr():
@@ -43,9 +28,7 @@ def get_noncestr():
 def get_timestamp():
     return str(int(time.time()))
 
-
 if __name__ == '__main__':
-    # import doctest
-    # doctest.testmod()
-    print(get_signature('sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg',
-                        'http://mp.weixin.qq.com?params=value'))
+    import doctest
+    doctest.testmod()
+

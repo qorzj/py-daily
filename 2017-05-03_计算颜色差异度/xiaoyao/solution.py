86ae3b2714969182ae6c69b675b2ecbcf8db496e
shawn <837364078@qq.com>
2017-05-04 11:44:48 +0800

    逍遥_拼音拆分_终终版

--- 2017-05-03_计算颜色差异度/xiaoyao/solution.py
+++ 2017-05-03_计算颜色差异度/xiaoyao/solution.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3.5
-from functools import reduce
 
 
 def segment(s):
     s = s[1:]
     index = 0
     while index < len(s) - 1:
-        yield s[index:index + 2]
+        yield int((s[index:index + 2]), 16)
         index += 2
 
 
@@ -17,13 +16,10 @@ def color_diff(c1, c2):
     >>> color_diff('#FF0000', '#FFFFFF')
     510
     """
-    return reduce(int.__add__, map(lambda x: abs(HextoInt(x[0]) - HextoInt(x[1])), zip(segment(c1), segment(c2))))
-
-
-def HextoInt(s):
-    return int(s, 16)
+    return sum(abs(x - y) for x, y in zip(segment(c1), segment(c2)))
 
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
